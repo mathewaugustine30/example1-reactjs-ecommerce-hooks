69c0c074f40ed8be50299150b1a9b11b3c370e6a
Mathew Augustine <Mathew.Augustine@ibm.com>
2024-02-28 17:40:09 +0530

    Applying validations on Login Page

--- src/Login.js
+++ src/Login.js
@@ -1,32 +1,138 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 
-let Login = () => {
+let Login = (props) => {
   var [email, setEmail] = useState("");
   var [password, setPassword] = useState("");
 
-//will get executed on initial render
-  useEffect(()=>{
-    document.title = "eCommerce - Login"
-    console.log(`renter user:${email} and password:${password}`)
-  },[]);
+  let [dirty, setDirty] = useState({
+    email: false,
+    password: false,
+  });
 
-//will get executed on initial render+every state changes
-  // useEffect(()=>{
-  //   console.log(`renter user:${email} and password:${password}`)
-  // });
+  let [errors, setErrors] = useState({
+    email: [],
+    password: [],
+  });
 
-////will get executed on initial render and when the state o password change
-  // useEffect(()=>{
-  //   console.log(`renter user:${email} and password:${password}`)
-  // },[password]);
+  let [loginMessage, setLoginMessage] = useState("");
 
-//will get executed on component unmount  -> cleanup code --> un handle the promise 
-  // useEffect(()=>{
-  //   return ()=>{
-  //     console.log(`renter user:${email} and password:${password}`);
+  //executes on each render (initial render & state updates)
+  useEffect(() => {
+    //console.log(email, password);
+  });
 
-  //   }
-  //});
+  //executes only on state updates of "email" only (and also with initial render)
+  useEffect(() => {
+    //validation on email only
+    if (email.indexOf("@") > 0) {
+      //console.log("valid");
+    } else {
+      //console.log("invalid");
+    }
+  }, [email]);
+
+  //executes only once - on initial render =  componentDidMount
+  useEffect(() => {
+    document.title = "Login - eCommerce";
+  }, []);
+
+  //executes only once - on component unmounting phase = componentWillUnmount
+  useEffect(() => {
+    //do something
+    return () => {
+      console.log("Component Unmount");
+    };
+  }, []);
+
+  //a function to validate email and password
+  let validate = () => {
+    //variable to store errorsData
+    let errorsData = {};
+
+    //email
+    errorsData.email = [];
+
+    //email can't blank
+    if (!email) {
+      errorsData.email.push("Email can't be blank");
+    }
+
+    //email regex
+    let validEmailRegex = /\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/;
+    if (email) {
+      if (!validEmailRegex.test(email)) {
+        errorsData.email.push("Proper email address is expected");
+      }
+    }
+
+    //password
+    errorsData.password = [];
+
+    //password can't blank
+    if (!password) {
+      errorsData.password.push("Password can't be blank");
+    }
+
+    //password regex
+    let validPasswordRegex = /((?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,15})/;
+    if (password) {
+      if (!validPasswordRegex.test(password)) {
+        errorsData.password.push(
+          "Password should be 6 to 15 characters long with at least one uppercase letter, one lowercase letter and one digit"
+        );
+      }
+    }
+
+    setErrors(errorsData);
+  };
+
+  useEffect(validate, [email, password]);
+
+  //When the user clicks on Login button
+  let onLoginClick = async () => {
+    //set all controls as dirty
+    let dirtyData = dirty;
+    Object.keys(dirty).forEach((control) => {
+      dirtyData[control] = true;
+    });
+    setDirty(dirtyData);
+
+    //call validate
+    validate();
+
+    if (isValid()) {
+      let response = await fetch(
+        `http://localhost:5000/users?email=${email}&password=${password}`,
+        { method: "GET" }
+      );
+      if (response.ok) {
+        //Status code is 200
+        let responseBody = await response.json();
+        if (responseBody.length > 0) {
+          props.history.replace("/dashboard");
+        } else {
+          setLoginMessage(
+            <span className="text-danger">Invalid Login, please try again</span>
+          );
+        }
+      } else {
+        setLoginMessage(
+          <span className="text-danger">Unable to connect to server</span>
+        );
+      }
+    }
+  };
+
+  let isValid = () => {
+    let valid = true;
+
+    //reading all controls from errors
+    for (let control in errors) {
+      if (errors[control].length > 0) valid = false;
+    }
+
+    return valid;
+  };
   return (
     <div className="row">
       <div className="col-lg-5 col-md-7 mx-auto">
@@ -52,10 +158,17 @@ let Login = () => {
                 value={email}
                 onChange={(event) => {
                   setEmail(event.target.value);
-                  console.log(email);
+                }}
+                onBlur={() => {
+                  setDirty({ ...dirty, email: true });
+                  validate();
                 }}
                 placeholder="Email"
               />
+
+              <div className="text-danger">
+                {dirty["email"] && errors["email"][0] ? errors["email"] : ""}
+              </div>
             </div>
             {/* email ends  */}
 
@@ -71,11 +184,27 @@ let Login = () => {
                 placeholder="Password"
                 onChange={(event) => {
                   setPassword(event.target.value);
-                  console.log(password);
+                }}
+                onBlur={() => {
+                  setDirty({ ...dirty, password: true });
+                  validate();
                 }}
               />
+
+              <div className="text-danger">
+                {dirty["password"] && errors["password"][0]
+                  ? errors["password"]
+                  : ""}
+              </div>
             </div>
             {/* password ends  */}
+          </div>
+
+          <div className="card-footer text-center">
+            <div className="m-1">{loginMessage}</div>
+            <button className="btn btn-success m-2" onClick={onLoginClick}>
+              Login
+            </button>
           </div>
         </div>
       </div>
